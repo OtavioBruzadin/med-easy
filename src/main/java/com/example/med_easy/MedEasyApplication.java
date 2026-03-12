c744a41f8b5b7172a1871c6eaeb7d80e6ff368b1
OtavioBruzadin <otavio.bruzadin@aioti.com.br>
2026-03-12 10:35:15 -0300

    Initial commit

---
+++ src/main/java/com/example/med_easy/MedEasyApplication.java
@@ -0,0 +1,13 @@
+package com.example.med_easy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedEasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/med_easy/MedEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.med_easy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
